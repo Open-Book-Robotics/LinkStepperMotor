53161e552bfcde99419558413954228dfb2ffb44
Sharwin Patil <sharwin24@gmail.com>
2023-11-10 01:04:10 -0500

    Added updateAccel function with s-curve acceleration profile

--- LinkStepperMotor.cpp
+++ LinkStepperMotor.cpp
@@ -23,12 +23,11 @@ void LinkStepperMotor::enable() { digitalWrite(this->enablePin, LOW); }
 void LinkStepperMotor::disable() { digitalWrite(this->enablePin, HIGH); }
 bool LinkStepperMotor::isEnabled() { return !digitalRead(this->enablePin); }
 
-bool LinkStepperMotor::readDirectionPin() { return digitalRead(this->dirPin); }
 int16_t LinkStepperMotor::getCurrentPosition() { return this->currentPosition; }
 int16_t LinkStepperMotor::getTargetPosition() { return this->targetPosition; }
 float LinkStepperMotor::getCurrentAngle() { return this->currentAngle; }
 uint16_t LinkStepperMotor::getSpeedSPS() { return this->currentSpeedSPS; }
-unsigned long LinkStepperMotor::getDelay() { return this->currentDelay; }
+unsigned long LinkStepperMotor::getPulseInterval() { return this->currentPulseInterval; }
 
 void LinkStepperMotor::calibrate(bool calibrationDirection, bool calibrationNO) {
 	if (this->calibrationPin == -1) { return; }
@@ -59,7 +58,7 @@ void LinkStepperMotor::update() {
 	// Determine the time since the last step
 	unsigned long currentTime = micros();
 	unsigned long timeSinceLastStep = currentTime - this->previousModulationTime;
-	if (timeSinceLastStep > this->currentDelay) {
+	if (timeSinceLastStep > this->currentPulseInterval) {
 		// Swap HIGH/LOW pulse on every occurance of the delay
 		this->isRunning = !this->isRunning;
 		this->isRunning ? digitalWrite(this->stepPin, HIGH) : digitalWrite(this->stepPin, LOW);
@@ -68,15 +67,66 @@ void LinkStepperMotor::update() {
 			this->currentPosition < this->targetPosition ? this->currentPosition++ : this->currentPosition--;
 		}
 		this->previousModulationTime = currentTime;
-		// Speed can be reassigned here to compute a new currentDelay depending on the progress of the movement
 	}
+}
+
+void LinkStepperMotor::updateAccel() {
+	// If the motor is not moving, we have reached the target
+	if (!this->isMoving()) { return; }
+
+	// Determine the time since the last step
+	unsigned long currentTime = micros();
+	unsigned long timeSinceLastStep = currentTime - this->previousModulationTime;
+	if (timeSinceLastStep > this->currentPulseInterval) {
+		// Swap HIGH/LOW pulse on every occurance of the delay
+		this->isRunning = !this->isRunning;
+		this->isRunning ? digitalWrite(this->stepPin, HIGH) : digitalWrite(this->stepPin, LOW);
+		// Increment/Decrement steps during LOW pulse in order to avoid missing steps
+		if (!this->isRunning && this->currentPosition != this->targetPosition) {
+			this->currentPosition < this->targetPosition ? this->currentPosition++ : this->currentPosition--;
+		}
+		this->previousModulationTime = currentTime;
+		// Speed can be reassigned here to compute a new currentPulseInterval depending on the progress of the movement
+		this->computeNewPulseInterval();
+	}
+}
+
+void LinkStepperMotor::computeNewPulseInterval() {
+	// Acceleration curve is split into 3 parts: acceleration, steady-state, deceleration
+	int totalSteps = abs(this->targetPosition - this->previousTargetPosition);
+	int stepsRemaining = abs(this->targetPosition - this->currentPosition);
+	int stepsCompleted = totalSteps - stepsRemaining;
+	int n1 = totalSteps / 3;
+	int n2 = totalSteps - n1;
+	uint16_t speedSPS = this->currentSpeedSPS;
+	// Determine which range we are in to apply the correct part of the acceleration curve
+	if (stepsCompleted <= n1) {
+		// Acceleration
+		// a(n) = k * n1 + a0
+		// v(n) = 0.5 * k * n^2 + a0 * n + v0
+		speedSPS = (0.5f * this->accelerationRate * pow(stepsCompleted, 2)) + (this->initialAcceleration * stepsCompleted) + this->initialSpeedSPS;
+	} else if (stepsCompleted >= n1 && stepsCompleted <= n2) {
+		// Steady-state
+		// a(n) = a_max = k * n1 + a0
+		// v(n) = (k * n1 + a0) * n - 0.5 * k * n1^2 + v0
+		speedSPS = ((this->accelerationRate * n1 + this->initialAcceleration) * stepsCompleted) - (0.5f * this->accelerationRate * pow(n1, 2)) + this->initialSpeedSPS;
+	} else { // (stepsCompleted >= n2)
+		// Deceleration
+		// a(n) = -k * n + k * n2 + a_max
+		// v(n) = -0.5 * k * n^2 + (k * n1 + k * n2 + a0) * n - 0.5 * k * (n1^2 + n2^2) - (k * n1 + a0) * (n1 - n2)
+		speedSPS = (-0.5f * this->accelerationRate * pow(stepsCompleted, 2))
+			+ (this->accelerationRate * n1 + this->accelerationRate * n2 + this->initialAcceleration) * stepsCompleted
+			- (0.5f * this->accelerationRate * (pow(n1, 2) + pow(n2, 2)))
+			- ((this->accelerationRate * n1 + this->initialAcceleration) * (n1 - n2));
+	}
+	this->setSpeedSPS(speedSPS);
 }
 
 void LinkStepperMotor::stepMotor() {
 	digitalWrite(this->stepPin, HIGH);
-	delayMicroseconds(this->currentDelay);
+	delayMicroseconds(this->currentPulseInterval);
 	digitalWrite(this->stepPin, LOW);
-	delayMicroseconds(this->currentDelay);
+	delayMicroseconds(this->currentPulseInterval);
 }
 
 bool LinkStepperMotor::isMoving() { return this->currentPosition != this->targetPosition; }
@@ -100,10 +150,8 @@ void LinkStepperMotor::setTargetPositionDegrees(float targetAngleDegrees) {
 	if (!isInRangeOfMotion(targetAngleDegrees)) {
 		// Flip the angle if it is out of range as well as the direction
 		targetAngleDegrees = targetAngleDegrees - 360.0f;
-		if (!isInRangeOfMotion(targetAngleDegrees)) {
-			// If the angle is still out of range, return
-			return;
-		}
+		// If the angle is still out of range, return
+		if (!isInRangeOfMotion(targetAngleDegrees)) { return; }
 	}
 	int16_t targetPosition = this->convertDegreesToSteps(targetAngleDegrees);
 	this->setTargetPosition(targetPosition);
@@ -116,7 +164,7 @@ void LinkStepperMotor::setSpeedRPM(float speedRPM) {
 
 void LinkStepperMotor::setSpeedSPS(uint16_t speedSPS) {
 	this->currentSpeedSPS = speedSPS;
-	this->currentDelay = this->getDelayFromSpeed(speedSPS);
+	this->currentPulseInterval = this->getPulseIntervalFromSpeed(speedSPS);
 }
 
 void LinkStepperMotor::setDirection(bool CW) {
--- LinkStepperMotor.hpp
+++ LinkStepperMotor.hpp
@@ -1,10 +1,11 @@
-#pragma once
+#ifndef LINKSTEPPERMOTOR_HPP
+#define LINKSTEPPERMOTOR_HPP
 /**
  * @file LinkStepperMotor.hpp
- * @author Sharwin Patil, RoboKits
+ * @author Sharwin Patil, OpenBook Robotics
  * @brief A Stepper Motor library for Arduino with a specific focus on robotic arm applications.
- * @version 1.0
- * @date 2023-08-15
+ * @version 2.0
+ * @date 2023-11-09
  *
  * @copyright Copyright (c) 2023
  *
@@ -64,36 +65,125 @@ public:
 		stepPin(stepPin), dirPin(dirPin), enablePin(enablePin), calibrationPin(calibrationPin),
 		stepsPerRevolution(stepsPerRevolution), gearRatio(gearRatio), minAngle(minAngle), maxAngle(maxAngle) {}
 
+	/**
+	 * @brief Enables the motor by setting the enable pin to LOW.
+	 *
+	 */
 	void enable();
+
+	/**
+	 * @brief Disables the motor by setting the enable pin to HIGH.
+	 *
+	 */
 	void disable();
+
+	/**
+	 * @brief Reads the enable pin to determine if the motor is enabled or not.
+	 *
+	 * @return true if the motor is enabled
+	 */
 	bool isEnabled();
 
+	/**
+	 * @brief Initializes the motor by setting the pin modes, initial pin states and enables the motor
+	 *
+	 */
 	void initialize();
+
+	/**
+	 * @brief Calibrates the motor by moving it in the specified direction until the calibration pin differs from its original state.
+	 *
+	 * @param calibrationDirection the direction in which to calibrate the motor [True -> CW, False -> CCW]
+	 * @param calibrationNO true if the calibration pin is normally open, false if the calibration pin is normally closed, defaults to true
+	 *
+	 */
 	void calibrate(bool calibrationDirection, bool calibrationNO = true);
 
-	bool readDirectionPin();
+	/**
+	 * @brief Gets the current motor position [steps]
+	 *
+	 * @return int16_t representing the current position of the motor [steps]
+	 */
 	int16_t getCurrentPosition();
-	int16_t getTargetPosition();
-	float getCurrentAngle();
-	uint16_t getSpeedSPS();
-	unsigned long getDelay();
 
+	/**
+	 * @brief Gets the target motor position [steps]
+	 *
+	 * @return int16_t representing the target position of the motor [steps]
+	 */
+	int16_t getTargetPosition();
+
+	/**
+	 * @brief Gets the current absolute angle of the motor [degrees] by converting the current position [steps] to degrees
+	 *
+	 * @return float representing the current angle of the motor [degrees]
+	 */
+	float getCurrentAngle();
+
+	/**
+	 * @brief Gets the current speed of the motor [steps/second]
+	 *
+	 * @return uint16_t representing the current speed of the motor [steps/second]
+	 */
+	uint16_t getSpeedSPS();
+
+	/**
+	 * @brief Gets the current pulse interval [microseconds] between steps
+	 *
+	 * @note This value is used as the delay between HIGH pulses to the step pin to control the speed of the motor.
+	 *
+	 * @return unsigned long representing the current pulse interval [microseconds]
+	 */
+	unsigned long getPulseInterval();
+
+	/**
+	 * @brief Determines if the given angle [degrees] is in the range of motion of the motor.
+	 *
+	 * @param angleDegrees The angle to check [degrees]
+	 * @return true if the angle is within the range of motion of the motor
+	 */
 	bool isInRangeOfMotion(float angleDegrees);
 
+	/**
+	 * @brief Sets the target motor position [steps] given the desired absolute angle [degrees]
+	 *
+	 * @param targetAngleDegrees The desired absolute angle of the motor [degrees]
+	 */
 	void setTargetPositionDegrees(float targetAngleDegrees);
+
+	/**
+	 * @brief Sets the motor speed to the given speed [RPM]
+	 *
+	 * @note The motor speed is controlled by the pulse interval [microseconds], so this function
+	 * 			 handles the conversion from [RPM] to [steps/second] to [microseconds] internally.
+	 *
+	 * @param speedRPM The desired speed of the motor [RPM]
+	 */
 	void setSpeedRPM(float speedRPM);
 
+	/**
+	 * @brief Determines if the motor is moving or not using the current and target positions.
+	 *
+	 * @return true if the motor is moving
+	 */
 	bool isMoving();
 
 	/**
-	 * @brief Asynchronous update function that utilizes the current delay and a micros() timer
+	 * @brief Asynchronous update function that utilizes the current pulse interval and a micros() timer
 	 * 				to determine when to step the motor.
 	 *
 	 */
 	void update();
 
 	/**
-	 * @brief Performs a single step using the current delay.
+	 * @brief Asynchronous update function that utilizes an acceleration profile to determine the current pulse interval
+	 * 				at each step and a micros() timer to determine when to step the motor.
+	 *
+	 */
+	void updateAccel();
+
+	/**
+	 * @brief Performs a single step using the current pulse interval.
 	 *
 	 */
 	void stepMotor();
@@ -153,19 +243,19 @@ private:
 	volatile unsigned long previousModulationTime = 0;
 
 	/**
-	 * @brief The current delay between steps [microseconds]
+	 * @brief The current pu;se interval between steps [microseconds]
 	 *
 	 * @note This is computed using the current speed and acceleration.
 	 */
-	unsigned long currentDelay = 0;
+	unsigned long currentPulseInterval = 0;
 
 	/**
 	 * @brief The current speed of the motor [steps/second]
 	 *
-	 * @note Defaults to 60 RPM [1 revolution/second], 3200 [steps/second]
+	 * @note Defaults to 60 RPM [1 revolution/second] which is the steps per revolution of the motor.
 	 *
 	 */
-	uint16_t currentSpeedSPS = 3200;
+	uint16_t currentSpeedSPS = this->stepsPerRevolution;
 
 	/**
 	 * @brief The current speed of the motor [revolutions/minute]
@@ -176,13 +266,46 @@ private:
 	float currentSpeedRPM = 60;
 
 	/**
-	 * @brief The current acceleration of the motor [steps/second^2]
+	 * @brief The initial speed of the motor when performing motion-profiled movement [steps/second]
 	 *
 	 */
-	uint16_t currentAcceleration = 0;
+	static constexpr int16_t initialSpeedSPS = 8000;
 
+	/**
+	 * @brief The k (acceleration rate) value used in the acceleration profile [steps/second^2]
+	 *
+	 */
+	static constexpr int16_t accelerationRate = 200;
+
+	/**
+	 * @brief The initial acceleration of the motor when performing motion-profiled movement [steps/second^2]
+	 *
+	 */
+	static constexpr int16_t initialAcceleration = 200;
+
+	/**
+	 * @brief Sets the target motor position [steps]
+	 *
+	 * @param targetPosition The desired target position of the motor [steps]
+	 */
 	void setTargetPosition(int16_t targetPosition);
+
+	/**
+	 * @brief Sets the direction of the motor [True -> CW, False -> CCW]
+	 *
+	 * @note This function writes to the digital pin to set the direction in addition to updating the internal field.
+	 *
+	 * @param CW The desired direction of the motor [True -> CW, False -> CCW]
+	 */
 	void setDirection(bool CW);
+
+	/**
+	 * @brief Sets the current motor speed [steps/second]
+	 *
+	 * @note This function internally computes the pulse interval [microseconds] and updates the internal field.
+	 *
+	 * @param speedSPS The desired speed of the motor [steps/second]
+	 */
 	void setSpeedSPS(uint16_t speedSPS);
 
 	/**
@@ -196,13 +319,30 @@ private:
 	int16_t convertDegreesToSteps(float degrees) const { return round((degrees / 360.0f) * this->stepsPerRevolution * this->gearRatio); }
 
 	/**
-	 * @brief Given a speed [steps/second], calculate the delay between steps [microseconds]
+	 * @brief Given a speed [steps/second], calculate the pulse interval between steps [microseconds]
 	 *
 	 * @param speed The speed of the motor [steps/second]
 	 *
-	 * @return unsigned long representing the delay between steps [microseconds]
+	 * @return unsigned long representing the pulse interval between steps [microseconds]
 	 */
-	static unsigned long getDelayFromSpeed(uint16_t speed) { return (unsigned long)(1000000.0f / speed); }
+	static unsigned long getPulseIntervalFromSpeed(uint16_t speed) { return (unsigned long)(1000000.0f / speed); }
 
+	/**
+	 * @brief Given a pulse interval [microseconds], calculate the speed [steps/second]
+	 *
+	 * @param pulseInterval The pulse interval between steps [microseconds] which determines the stepper motor speed
+	 * @return uint16_t representing the speed of the motor [steps/second]
+	 */
+	static uint16_t getSpeedFromPulseInterval(unsigned long pulseInterval) { return (uint16_t)(1000000.0f / pulseInterval); }
+
+	/**
+	 * @brief Computes the current angle [degrees] using the current motor position [steps]
+	 * 				and updates the internal field.
+	 *
+	 */
 	void updateCurrentAngle();
+
+	void computeNewPulseInterval();
 };
+
+#endif // LINKSTEPPERMOTOR_HPP
